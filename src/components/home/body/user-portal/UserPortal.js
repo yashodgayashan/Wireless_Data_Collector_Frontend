be70763f3b221ba992721b7e275720eaf49371a8
yashodgayashan <yashodgayashan@gmail.com>
2020-03-08 20:54:32 +0530

    Added user portal

---
+++ src/components/home/body/user-portal/UserPortal.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const UserPortal = () => {
+  return (
+    <Fragment>
+      <h1>This is User UserPortal</h1>
+    </Fragment>
+  );
+};
+
+export default UserPortal;
