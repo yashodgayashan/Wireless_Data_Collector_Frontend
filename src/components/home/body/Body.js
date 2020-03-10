2504cd5c5ea9fa9ee80d0901e0ef256d34d75510
yashodgayashan <yashodgayashan@gmail.com>
2020-03-10 17:14:55 +0530

    Added routing for login, signup and user-portal

--- src/components/home/body/Body.js
+++ src/components/home/body/Body.js
@@ -1,9 +1,19 @@
 import React, { Fragment } from "react";
+import Login from "./login/Login";
+import SignUp from "./signup/Signup";
+import UserPortal from "./user-portal/UserPortal";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const Body = () => {
   return (
     <Fragment>
-      <h1>Body</h1>
+      <Router>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/sign-up" component={SignUp} />
+          <Route path="/user-portal" component={UserPortal} />
+        </Switch>
+      </Router>
     </Fragment>
   );
 };
